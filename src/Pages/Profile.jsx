547719c1c573494c59295fa93920587aa9faecbb
Ben <benjamin.valadilaine@gmail.com>
2020-11-05 16:27:15 +0100

    posts can be seen on our own profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -1,17 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Cookies from "js-cookie";
-import NewPost from '../components/NewPost';
-import UpdateProfile from '../components/UpdateProfile';
+import jwt_decode from "jwt-decode";
+import NewPost from "../components/NewPost";
+import UpdateProfile from "../components/UpdateProfile";
+import Post from "../components/Post/";
 
 const Profile = () => {
-	const [userToken, setUserToken] = useState(Cookies.get("token"));
 	const [profileData, setProfileData] = useState({});
 	const [toggleUpdate, setToggleUpdate] = useState(false);
+	const [posts, setPosts] = useState();
+	const userToken = Cookies.get("token");
+	const decoded = jwt_decode(userToken);
 
 	const showUpdateForm = (e) => {
 		e.preventDefault();
 		setToggleUpdate(!toggleUpdate);
-	}
+	};
+
+	const fetchPosts = () => {
+		fetch(
+			`https://my-pasteque-space.herokuapp.com/posts?user.id=${decoded.id}`,
+			{
+				method: "get",
+				headers: {
+					Authorization: `Bearer ${userToken}`,
+					"Content-Type": "application/json",
+				},
+			},
+		)
+			.then((response) => response.json())
+			.then((response) => {
+				setPosts(response);
+				console.log(response);
+			});
+	};
 
 	useEffect(() => {
 		if (userToken) {
@@ -25,6 +47,7 @@ const Profile = () => {
 				.then((response) => response.json())
 				.then((response) => {
 					setProfileData(response);
+					fetchPosts();
 				});
 		}
 	}, []);
@@ -38,7 +61,9 @@ const Profile = () => {
 					<p>{profileData.email}</p>
 				</div>
 			)}
-			<button onClick={(e) => showUpdateForm(e)}>Mettre à jour mon profil</button>
+			<button onClick={(e) => showUpdateForm(e)}>
+				Mettre à jour mon profil
+			</button>
 			{toggleUpdate && (
 				<div>
 					<hr />
@@ -46,7 +71,17 @@ const Profile = () => {
 				</div>
 			)}
 			<hr />
-      <NewPost />
+			<NewPost />
+			<hr />
+			{posts &&
+				posts.map((post) => (
+					<Post
+						id={post.id}
+						text={post.text}
+						user={post.user.username}
+						userId={post.user.id}
+					/>
+				))}
 		</section>
 	);
 };
